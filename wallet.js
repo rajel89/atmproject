2dc4050a9ebf93b6e9cd21ab9daa5b5546ec7b43
rajel89 <rajah.ello@yahoo.com>
2021-07-05 16:44:44 -0500

    attempting to make the wallet.js contains and receives cash for ATM deposit and withdraw functions.

--- wallet.js
+++ wallet.js
@@ -1,6 +1,13 @@
+//const atm = require("./atm"); //to import withdrawal or deposit int othe wallet.
+
 let withdrawAmount = 0;
 
 let depositAmount = 0;
+
+
+
+
+
 
 module.exports = {
   withdAmount: withdrawAmount,
